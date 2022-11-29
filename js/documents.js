30be4fb582a698412537162b000444424bb4aa09
unknown <mengqingbing@qianxin.com>
2022-11-29 09:46:39 +0800

    new

---
+++ js/documents.js
@@ -0,0 +1,24 @@
+function getDocName(){
+    let docName = "";
+    let key = "id";
+    let reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)");
+    let r = window.location.search.substr(1).match(reg);
+    docName_t = decodeURI(r[2]);
+    docName = docName_t.split('\'')[1];
+    return docName;
+}
+
+function PageInit(){
+    let filename = getDocName();
+    $("#top").html(filename.split(".")[0]);
+    $("title").html(filename.split(".")[0]);
+    let uri = "./markdown/"+filename;
+    $.ajax({
+        url: uri, 
+        async: true, 
+        success: function(res){
+            $("#doc").html(res);
+            $("#num").html("总字数:"+res.length.toString());
+        }});
+}
+
